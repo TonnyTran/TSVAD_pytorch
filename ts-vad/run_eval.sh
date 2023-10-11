625a794299ded5bbc762823f10c644d8a9076d5b
#AZMAT ADNAN# <adnan002@asp2a-login-nscc01.head.cm.asp2a.nscc.sg>
2023-10-11 22:02:41 +0800

    modify sh and pbs files

--- ts-vad/run_eval.sh
+++ ts-vad/run_eval.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 python main.py \
---train_list /data08/alimeeting/Train_Ali_far/ts_Train.json \
---eval_list /data08/alimeeting/Eval_Ali_far/ts_Eval.json \
---train_path /data08/alimeeting/Train_Ali_far \
---eval_path /data08/alimeeting/Pseduo_Ali_far \
+--train_list /home/users/ntu/adnan002/scratch/alimeeting/Train_Ali_far/ts_Train.json \
+--eval_list /home/users/ntu/adnan002/scratch/alimeeting/Eval_Ali_far/ts_Eval.json \
+--train_path /home/users/ntu/adnan002/scratch/alimeeting/Train_Ali_far \
+--eval_path /home/users/ntu/adnan002/scratch/alimeeting/Eval_Ali_far \
 --save_path exps/res23 \
 --rs_len 4 \
 --test_shift 0.5 \
@@ -13,4 +13,4 @@ python main.py \
 --threshold 0.50 \
 --n_cpu 12 \
 --eval \
---init_model pretrained_models/ts-vad.model \
+--init_model /home/users/ntu/adnan002/scratch/TSVAD_pytorch/ts-vad/pretrained_models/ts-vad.model \
--- ts-vad/run_train.sh
+++ ts-vad/run_train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python main.py \
---train_list /data08/alimeeting/Train_Ali_far/ts_Train.json \
---eval_list /data08/alimeeting/Eval_Ali_far/ts_Eval.json \
---train_path /data08/alimeeting/Train_Ali_far \
---eval_path /data08/alimeeting/Eval_Ali_far \
+python /home/users/ntu/adnan002/scratch/TSVAD_pytorch/ts-vad/main.py \
+--train_list /home/users/ntu/adnan002/scratch/DIHARD3/third_dihard_challenge_dev/data/ts_dev.json \
+--eval_list /home/users/ntu/adnan002/scratch/DIHARD3/third_dihard_challenge_eval/data/ts_eval.json \
+--train_path /home/users/ntu/adnan002/scratch/DIHARD3/third_dihard_challenge_dev/data \
+--eval_path /home/users/ntu/adnan002/scratch/DIHARD3/third_dihard_challenge_eval/data \
 --save_path exps/res23 \
 --warm_up_epoch 10 \
 --batch_size 40 \
