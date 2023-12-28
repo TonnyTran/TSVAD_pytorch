b4a3dd03d96ab409cacad52a564f97f15e4b8f7e
#AZMAT ADNAN# <adnan002@asp2a-login-nscc01.head.cm.asp2a.nscc.sg>
2023-12-28 19:43:39 +0800

    minor changes

--- ts-vad/trainer.py
+++ ts-vad/trainer.py
@@ -116,17 +116,15 @@ class trainer(nn.Module):
 				rttm.write(line)
 		rttm.close()
 		print('\n')
-		print ('E$$$$')
 		print (args.rttm_save_path)
-		print ('E$$$$')
 
-		out = subprocess.check_output(['perl', 'tools/SCTK-2.4.12/src/md-eval/md-eval.pl', '-c 0.25', '-s %s'%(args.rttm_save_path), '-r data/DIHARD3/third_dihard_challenge_eval/data/rttm/all.rttm'])
+		rttm_file_path = args.eval_path + "/rttm/all.rttm"
+		out = subprocess.check_output(['perl', 'tools/SCTK-2.4.12/src/md-eval/md-eval.pl', '-c 0.25', '-s %s'%(args.rttm_save_path), '-r ' + rttm_file_path])
 		out = out.decode('utf-8')
 		DER, MS, FA, SC = float(out.split('/')[0]), float(out.split('/')[1]), float(out.split('/')[2]), float(out.split('/')[3])
 		print("DER %2.2f%%, MS %2.2f%%, FA %2.2f%%, SC %2.2f%%\n"%(DER, MS, FA, SC))
 		args.score_file.write("Eval full 0.25: %d epoch, DER %2.2f%%, MS %2.2f%%, FA %2.2f%%, SC %2.2f%%, LOSS %f\n"%(args.epoch, DER, MS, FA, SC, nloss/num))
-
-		out = subprocess.check_output(['perl', 'tools/SCTK-2.4.12/src/md-eval/md-eval.pl', '-c 0.00', '-s %s'%(args.rttm_save_path), '-r data/DIHARD3/third_dihard_challenge_eval/data/rttm/all.rttm'])
+		out = subprocess.check_output(['perl', 'tools/SCTK-2.4.12/src/md-eval/md-eval.pl', '-c 0.25', '-s %s'%(args.rttm_save_path), '-r ' + rttm_file_path])
 		out = out.decode('utf-8')
 		DER, MS, FA, SC = float(out.split('/')[0]), float(out.split('/')[1]), float(out.split('/')[2]), float(out.split('/')[3])
 		print("DER %2.2f%%, MS %2.2f%%, FA %2.2f%%, SC %2.2f%%\n"%(DER, MS, FA, SC))
